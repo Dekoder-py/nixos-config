69c87a2eba7b0da63732e8ddd37d74edd474cb3b
Dekoder-py <dekod3r.py@gmail.com>
2025-07-15 16:44:49 +1200

    add cachix for hyprland

--- configuration.nix
+++ configuration.nix
@@ -90,6 +90,12 @@
     git
   ];
 
+  nix.settings = {
+    substituters = [ "https://hyprland.cachix.org" ];
+    trusted-substituters = [ "https://hyprland.cachix.org" ];
+    trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+  };
+
   environment.variables.EDITOR = "nvim";
 
   # Some programs need SUID wrappers, can be configured further or are
